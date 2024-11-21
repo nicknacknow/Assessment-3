65fe200248762b979e779841578f18d038d0189f
nick <nick@email.com>
2024-11-21 09:50:12 +0000

    work on c instruction

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 // Run with `java Main.java test`
 import java.io.*;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static java.util.Map.entry; // use this just to make mapping encoding stuff less ugly
 
 public class Main {
     public static boolean iUsedAi() {
@@ -202,7 +207,72 @@ class Assembler {
         output.write('\n');
     }
 
+    private String IntTo15BitBinary(int n) {
+        int num = n;
 
+        String binary = "";
+        for (int i = 14; i >= 0; i--)
+        {
+            int int_max = (int)Math.pow(2, i);
+
+            if (num >= int_max) {                
+                binary += "1";
+                num -= int_max;
+            }
+            else binary += "0";
+        }
+
+        return binary;
+    }
+
+    // is there a better way to do this? e.g. each c bit has specific meaning
+    Map<String, String> compBitDict = Map.ofEntries(
+        entry("0", "101010"),
+        entry("1", "111111"),
+        entry("-1", "111010"),
+        entry("D", "001100"),
+        entry("A", "110000"),
+        entry("!D", "001101"),
+        entry("!A", "110001"),
+        entry("D+1", "001111"),
+        entry("A+1", "110111"),
+        entry("D-1", "001110"),
+        entry("A-1", "110010"),
+        entry("D+A", "000010"),
+        entry("D-A", "010011"),
+        entry("A-D", "000111"),
+        entry("D&A", "000000"),
+        entry("D|A", "010101")
+    );
+
+    private String FormCompBitString(String comp) {
+        boolean contains_m = comp.contains("M");
+        if (contains_m) { // little hack so i dont have repeat entries in map
+            comp = comp.replaceAll("M", "A");
+        }
+        // ternary operator, if it contains m, we want a=1 otherwise a=0
+        return ((contains_m ? "1" : "0") + compBitDict.get(comp));
+    }
+
+    Map<String, String> destBitDict = Map.ofEntries(
+        entry("null", "000"),  // The value is not stored
+        entry("M", "001"),     // RAM[A]
+        entry("D", "010"),     // D register
+        entry("MD", "011"),    // RAM[A] and D register
+        entry("A", "100"),     // A register
+        entry("AM", "101"),    // A register and RAM[A]
+        entry("AD", "110"),    // A register and D register
+        entry("AMD", "111")    // A register, RAM[A], and D register
+    );
+
+    private String FormDestBitString(String dest) {
+        return destBitDict.get(dest);
+    }
+
+    // we assume sComp contains 'a' bit. (a=0 or a=1 for comp)
+    private String FormCInstruction(String sComp, String sDest, String sJump) {
+        return "111" + sComp + sDest + sJump;
+    }
 
     public void assemble() throws IOException {
         
@@ -216,9 +286,7 @@ class Assembler {
             String operands_string = string.substring(3, string.length());
             operands_string = operands_string.replaceAll("\\s+", ""); // remove whitespace
             
-            List<String> operands = Arrays.asList(
-                operands_string.strip().split(",")
-            );
+            String[] operands = operands_string.split(",");
             
             // need to check for jmps such as jgt, jlt, jeq, ... just check first char=j then do switch
 
@@ -227,6 +295,35 @@ class Assembler {
                     System.out.println("ldr: ");
                     for (String op : operands) {
                         System.out.println("    " + op);
+                    }
+
+                    if (operands[0].equals("A")) {
+                        String op2 = operands[1];
+                        int val = Integer.parseInt(op2.substring(1));
+
+                        // call IntTo15BitBinary (& find a better name) and write to output method
+                        output("0" + IntTo15BitBinary(val));
+                    }
+                    else {
+                        String target = operands[0];
+                        String src = operands[1];
+                        
+                        src = src.replaceAll("\\(A\\)", "M");
+                        // code above does code below --- will A be the only value that appears in parentheses? 
+
+                        /*if (!src.equals(src.replaceAll("[()]", ""))) { // this means parentheses are present, indicating we need to read the memory of src
+                            if (src.equals("(A)")) src = "M";
+                            else {
+                                System.out.println("check this out!!!!! " + src);
+                            }
+                        }*/
+                        
+                        String comp = FormCompBitString(src);
+                        String dest = FormDestBitString(target);
+
+                        // ensure this is correct
+                        output(FormCInstruction(comp, dest, "000"));
+                        //System.out.println("target: " + target + "(" + dest + ") src: " + src + "(" + comp + ")");
                     }
 
                     break;
